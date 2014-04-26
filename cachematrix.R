87a4ca502667bdd0ebb2707ee9c0df6d17833908
Philip Graff <philip.graff@ligo.org>
2014-04-25 22:21:49 -0400

    Completed functions for assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create an object for a cached matrix inverse.
+## They create an object that stores the matrix and its inverse
+## and contain a function to solve for the inverse, recalling
+## the cached one if already computed for the input matrix.
 
-## Write a short comment describing this function
+## Creates the matrix inverse cache object. Stores the matrix
+## and its cached inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        y <- NULL
+        set <- function(z) {
+                x <<- z
+                y <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(z) y <<- z
+        getInverse <- function() y
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Solves for the inverse of a matrix.
+## Uses cached solution if already computed for input matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        y <- x$getInverse()
+        if ( !is.null(y) ) {
+                # Inverse already computed: return from cache
+                message("Retrieving cached inverse")
+                return(y)
+        } else {
+                # Need to compute inverse: get matrix and find inverse,
+                # store and return solution
+                m <- x$get()
+                y <- solve(m)
+                x$setInverse(y)
+                return(y)
+        }
 }
